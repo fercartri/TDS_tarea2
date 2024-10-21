657d0e9eec389a4406c9029683851f933fd1701c
fercarm <fernando.carmona22@estudiantes.uva.es>
2024-10-21 18:14:02 +0200

    test: añade test gestor válido

--- tarea2/src/main/java/uva/tds/GestorFacturas.java
+++ tarea2/src/main/java/uva/tds/GestorFacturas.java
@@ -1,6 +1,26 @@
 package uva.tds;
 
+import java.time.LocalDate;
+
 public class GestorFacturas {
 
-    
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String nombre;
+
+
+    public GestorFacturas(LocalDate inicio, LocalDate fin, String nombre){
+
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
 }
--- tarea2/src/test/java/uva/tds/GestorFacturasTest.java
+++ tarea2/src/test/java/uva/tds/GestorFacturasTest.java
@@ -1,12 +1,19 @@
 package uva.tds;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 public class GestorFacturasTest {
 
     @Test
     void testGestorFacturasValido(){
-        
+        GestorFacturas g = new GestorFacturas(LocalDate.of(2024, 12, 22), LocalDate.of(2025, 4, 1), "Nombre");
+        assertEquals(g.getFechaInicio(), LocalDate.of(2024, 12, 22));
+        assertEquals(g.getFechaFin(), LocalDate.of(2025, 4, 1));
+        assertEquals(g.getNombre(), "Nombre");
     }
 
 
