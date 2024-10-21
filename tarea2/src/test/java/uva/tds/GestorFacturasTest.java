93c4065849a33d444df02c7edac47c999dfa3117
fercarm <fernando.carmona22@estudiantes.uva.es>
2024-10-21 18:18:10 +0200

    test: añade test fecha fin null

--- tarea2/src/test/java/uva/tds/GestorFacturasTest.java
+++ tarea2/src/test/java/uva/tds/GestorFacturasTest.java
@@ -24,5 +24,12 @@ public class GestorFacturasTest {
         });
     }
 
+    @Test
+    void testGestorFacturaNoValidoFechaFinNull(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            GestorFacturas g = new GestorFacturas(LocalDate.of(2024, 12, 22), null, "Nombre");
+        });
+    }
+
 
 }
