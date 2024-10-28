c4de1d0b8be31812521e766a5ec2182f0d336235
fercarm <fernando.carmona22@estudiantes.uva.es>
2024-10-28 17:59:56 +0100

    other: refactor de las clases test

--- tarea2/src/test/java/uva/tds/FacturaTest.java
+++ tarea2/src/test/java/uva/tds/FacturaTest.java
@@ -1,7 +1,6 @@
 package uva.tds;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
